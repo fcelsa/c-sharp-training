d0a17193be3d60e993e5b349a8e622fedb9c3cd0
fcelsa <fabio.celsa@gmail.com>
2023-02-05 13:09:40 +0100

    check iso6346 container ship

---
+++ iso6346-check/iso6346-check/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace iso6346_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    static bool ChIso6346(cntr)
+    {
+        return true;
+
+    }
+}
