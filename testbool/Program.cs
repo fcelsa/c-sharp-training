9fbda5d7a8432f2c0213da1ba572e1f2cc4f4721
fcelsa <fabio.celsa@gmail.com>
2026-01-10 15:45:24 +0100

    testbool

---
+++ testbool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace testbool
+{
+    internal class Program
+    {
+        static bool _ = true;
+        static int __ = Convert.ToByte(_) + 64;
+        static string ___ = ((char)__ ).ToString();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(___);
+        }
+
+    }
+
+}
